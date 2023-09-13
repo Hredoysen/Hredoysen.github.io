8ac8005441e6ae4e2e20afac73111367cccb528f
Hredoysen <shovon.sen60@gmail.com>
2023-09-14 04:46:43 +0600

    feat:deploy

--- src/components/ui/Card/card.tsx
+++ src/components/ui/Card/card.tsx
@@ -1,17 +1,16 @@
 import classNames from "classnames";
 import React from "react";
-import { Image, Typography } from "antd";
+import { Image } from "antd";
 import logo from "../../../assets/logo (1).png";
 import "./styles.scss";
 
-const { Title } = Typography;
 interface ButtonProp {
   classname?: string;
   label?: string | React.ReactNode;
   onclick?: () => void;
   ondblClick?: () => void;
 }
-function Card({ classname, label, onclick, ondblClick }: ButtonProp) {
+function Card({ classname, onclick, ondblClick }: ButtonProp) {
   const classes = classNames("card-wrapper", classname);
   return (
     <div className={classes} onClick={onclick} onDoubleClick={ondblClick}>
